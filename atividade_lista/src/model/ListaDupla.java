f364d23af3deb11fc093810ba8bc56814a6dd8e3
Kauan Rossi <164578708+Sadzin1603@users.noreply.github.com>
2026-03-11 14:50:43 -0300

    Logica reimaginada e reescrita

---
+++ atividade_lista/src/model/ListaDupla.java
@@ -0,0 +1,115 @@
+package model;
+
+import model.ListaSimples;
+import model.ListaSimples.No;
+
+public class ListaDupla {
+	class No{
+		private ListaSimples lista;
+		private String letra;
+		private No proximo;
+		private No anterior;
+		
+		public ListaSimples getValor() {
+			return lista;
+		}
+		public void setValor(ListaSimples lista) {
+			this.lista = lista;
+		}
+		public String getLetra() {
+			return letra;
+		}
+		public void setLetra(String letra) {
+			this.letra = letra;
+		}
+		public No getProximo() {
+			return proximo;
+		}
+		public void setProximo(No proximo) {
+			this.proximo = proximo;
+		}
+		public No getAnterior() {
+			return anterior;
+		}
+		public void setAnterior(No anterior) {
+			this.anterior = anterior;
+		}
+	}
+	
+	private No inicio;
+	
+	//ADD, vai receber o nome e a primeira letra, tem q ver se ja tem um nó com essa letra 
+	///e se tiver adicionar o nome nela, se não criar um nó com a letra e adicionar o nome depois
+	
+	public void add(String nome,String letra) {
+		if(this.inicio == null) { //não tem nenhuma letra
+			No novo = new No();
+			novo.letra = letra;
+			ListaSimples lista = new ListaSimples();
+			novo.lista= lista;
+			novo.lista.append(nome);
+			this.inicio = novo;
+		}else {//se ja tem alguma letra
+			if(verificaNos(letra)) {//se ja tem um no com essa letra
+				No existente = getLast(letra);
+				
+				existente.lista.append(nome);
+			}else {
+				No novo = new No();
+				No ultimo = getLast("");
+				novo.letra = letra;
+				ListaSimples lista = new ListaSimples();
+				novo.lista= lista;
+				novo.lista.append(nome);
+				novo.setAnterior(ultimo);
+				ultimo.setProximo(novo);
+			}
+		}
+	}
+	
+	
+	public Boolean verificaNos(String letra) {
+		No buffer = this.inicio;
+		Boolean res = false;
+		do {
+			if(buffer.letra.equals(letra)) {
+				res = true;
+			}
+			buffer.getProximo();
+		}while(buffer.getProximo()!=null);
+		return res;
+		
+			
+	}
+	
+	public No getLast(String letra) {
+		//preguiça de tratar erro
+		//if(inicio == null) {
+		//	return 0;
+		//}
+		if(letra.isEmpty()) {
+			No buffer = inicio;
+			while(buffer.proximo!=null) {
+				buffer = buffer.getProximo();
+			}
+			return buffer;
+		}else {
+			No buffer = inicio;
+			while(buffer.proximo!=null) {
+				if(buffer.letra.equals(letra)) {
+					return buffer;
+				}
+				buffer = buffer.getProximo();
+			}
+			return buffer;
+		}
+	}
+	
+	@Override
+	public String toString() {
+		String text ="[";
+		text+=this.inicio.getLetra();
+		text+="]";
+		return text;
+	}
+}
--- atividade_lista/src/view/View.java
+++ atividade_lista/src/view/View.java
@@ -1,28 +1,15 @@
 package view;
 
-import javax.swing.*;
-import controller.Controlador;
+import controller.Controller;
 
 public class View {
-    public static void main(String[] args){
-        try{
-            Controlador c = new Controlador();
-            
-            
-            String nome = "kauan";
-            System.out.println(c.cadastra(nome));
-            nome = "karla";
-            System.out.println(c.cadastra(nome));
-            nome = "rennan";
-            System.out.println(c.cadastra(nome));
-            nome = "rayssa";
-            System.out.println(c.cadastra(nome));
-                
-            
 
-            
-        }catch( Exception e ){
-            e.printStackTrace();
-        }
-    }
+	public static void main(String[] args) {
+		Controller c = new Controller();
+	
+		c.addPessoa("Pedro");
+		c.addPessoa("Paulo");
+		
+	}
+
 }
